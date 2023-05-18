19f5e920fca6c54fb61054d4ab75aee2a05038b4
jkmg <jcmguga@gmail.com>
2023-05-18 12:56:43 +0200

    Iris

--- src/main.py
+++ src/main.py
@@ -1,56 +1,54 @@
-from sklearn.preprocessing import PolynomialFeatures
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
-from joblib import load
-#from pycaret.regression import *
+import pickle
+from sklearn.linear_model import LogisticRegression
 
-st.write(""" ### Determination of STD NMR Binding Epitopes using the Reduced Dataset Approach from the STD factors at 0.75 and 6 s """)
+st.write("""
+# Iris plants Prediction App
+This app predicts the type of iris plant ('Iris-setosa', 'Iris-versicolor' and 'Iris-virginica') from the paramters
+sepal length, sepal width and petal width in cm. 
+Data to generate the model were obtained from Kaggle (https://www.kaggle.com/uciml/iris).
+""")
 
-st.sidebar.header('Analytical Approach')
+st.sidebar.header('User Input Features')
 
 # Collects user input features into dataframe
-uploaded_file1 = st.sidebar.file_uploader("Upload a CSV File", type=["csv"])
-if uploaded_file1 is not None:
-    df = pd.read_csv(uploaded_file1, header = None)
-    df.columns=['Proton', 'STD_0p75s', 'STD_6s']
-    df['ksat'] = (- np.log((df.STD_6s - df.STD_0p75s)/df.STD_6s))/0.75
-    df['STD0'] = df.ksat*df.STD_6s
-    df['Epitope'] = df.STD0/(np.max(df.STD0))*100
-    df.drop(['STD_0p75s', 'STD_6s'], inplace=True, axis=1)
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        SepalLengthCm = st.sidebar.slider('Sepal length (cm)', 0.0,10.0,5.1)
+        SepalWidthCm = st.sidebar.slider('Sepal width (cm)', 0.0,5.0,3.5)
+        PetalWidthCm = st.sidebar.slider('Petal width (cm)', 0.0,5.0,0.2)
+        data = {'SepalLengthCm': SepalLengthCm,
+                'SepalWidthCm': SepalWidthCm,
+                'PetalWidthCm': PetalWidthCm}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input = user_input_features()
+
+df = input[:1] # Selects only the first row (the user input data)
 
 # Displays the user input features
-st.subheader('Analytical Approach')
+st.subheader('User Input features')
 
-if uploaded_file1 is not None:
+if uploaded_file is not None:
     st.write(df)
 else:
-    st.write('Awaiting CSV file to be uploaded')
+    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+    st.write(df)
 
-#st.sidebar.header('ML Approach')
+# Load Logistic Regression classification model
+load_lr = pickle.load(open('iris_lr.pkl', 'rb'))
+# Logistic Regression Prediction
+prediction_lr = load_lr.predict(df)
+prediction_proba_lr = load_lr.predict_proba(df)
 
-# Collects user input features into dataframe
-#uploaded_file2 = st.sidebar.file_uploader("Upload a CSV File (without column headers) containing 3 columns, the first with the proton names, then the experimental STD factors at 0.75s and finally the STD factros at 6s", type=["csv"])
-#if uploaded_file2 is not None:
-#    poly = joblib.load('/home/usuario/Documentos/Marie_Curie/Proyectos/STD_ML/Model_Deployment/polynomial_features_stdml_model')
-#    model=joblib.load("/home/usuario/Documentos/Marie_Curie/Proyectos/STD_ML/Model_Deployment/extra_trees_best_sklearn_regressor")
-#    df2 = pd.read_csv(uploaded_file2, header = None)
-#    df2.rename(columns={0: 'Proton', 1: 'STD_0p75s', 2:'STD_6s'}, inplace=True)
-#    df2["feature1"] = df2.STD_0p75s/df2.STD_6s
-#    df2["feature2"] = np.exp(df2.STD_0p75s/df2.STD_6s)
-#    dfval_poly = poly.transform(df2.loc[:,"STD_0p75s":"feature2"])
-#    dfval_poly = pd.DataFrame(dfval_poly)
-#    pred = model.predict(dfval_poly)
-#    pred = pd.DataFrame(pred, columns=['STD0_ML'])
-#    pred['Proton'] = df2.Proton.values
-#    pred = pred.reindex(columns=['Proton', 'STD0_ML'])
-#    pred['Epitope_ML'] = pred.STD0_ML/(np.max(pred.STD0_ML))*100
+st.subheader('Logistic Regression Prediction')
+iris_species = np.array(['Iris-setosa','Iris-versicolor','Iris-virginica'])
+st.write(iris_species[prediction_lr])
 
-# Displays the user input features
-#st.subheader('ML Approach')
-
-#if uploaded_file2 is not None:
-#    st.write(pred)
-#else:
-#    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+st.subheader('Logistic Regression Prediction Probability')
+st.write(prediction_proba_lr)
