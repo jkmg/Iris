91572384e4ddd359938dd2cd8baf8dfe224e3db3
jkmg <jcmguga@gmail.com>
2023-09-06 16:36:57 +0200

    stdml

--- src/main.py
+++ src/main.py
@@ -1,27 +1,63 @@
 from sklearn.preprocessing import PolynomialFeatures
 import streamlit as st
+import st_aggrid
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+from streamlit_ace import st_ace
 import pandas as pd
 import numpy as np
 import joblib
 from joblib import load
-#from pycaret.regression import *
 
-st.write(""" ### Determination of STD NMR Binding Epitopes using the Reduced Dataset Approach from the STD factors at 0.75 and 6 s """)
+st.write(""" ### Determination of STD NMR Binding Epitopes using the Reduced Dataset Approach from STD factors at 2 saturation times """)
 
-st.sidebar.header('Analytical Approach')
+st.sidebar.header('Reduced Dataset Approach')
 
 # Collects user input features into dataframe
-uploaded_file1 = st.sidebar.file_uploader("Upload a CSV File", type=["csv"])
+uploaded_file1 = st.sidebar.file_uploader("**Upload a CSV File (without column headers) containing 3 columns, the first with the proton names, then the experimental STD factors at short (typically 0.5, 0.75 or 1 s; 0.75 s is recommended) and large (typically 6 or 8 s) saturation times**", type=["csv"])
+tsat_short = st.sidebar.number_input("**Enter the Short Saturation Time, in seconds**", value = 0.75, min_value=0.50, max_value=1.00)
 if uploaded_file1 is not None:
     df = pd.read_csv(uploaded_file1, header = None)
-    df.columns=['Proton', 'STD_0p75s', 'STD_6s']
-    df['ksat'] = (- np.log((df.STD_6s - df.STD_0p75s)/df.STD_6s))/0.75
-    df['STD0'] = df.ksat*df.STD_6s
+    df.columns=['Proton', 'STD_short_tsat', 'STD_long_tsat']
+    df['ksat'] = (- np.log((df.STD_long_tsat - df.STD_short_tsat)/df.STD_long_tsat))/tsat_short
+    df['STD0'] = df.ksat*df.STD_long_tsat
     df['Epitope'] = df.STD0/(np.max(df.STD0))*100
-    df.drop(['STD_0p75s', 'STD_6s'], inplace=True, axis=1)
+    df.drop(['STD_short_tsat', 'STD_long_tsat'], inplace=True, axis=1)
+else:
+    num_rows = st.number_input("Number of Ligand Protons for which you are going to input STD factors", 1, 50, 5)
+    def aggrid_interactive_table(num_rows):
+        data = pd.DataFrame(columns=["Proton_Name", "STD_short_tsat", "STD_long_tsat"], index=range(num_rows))
+        # Create grid options for interactive table
+        options = GridOptionsBuilder.from_dataframe(data, enableRowGroup=True, enableValue=True, enablePivot=True)
+        options.configure_side_bar()
+        #grid_options = options.build()
+        options.configure_selection("single")
+        selection = AgGrid(data, enable_enterprise_modules=True, gridOptions=options.build(), theme="material", update_mode=GridUpdateMode.MODEL_CHANGED, allow_unsafe_jscode=True, key='table')
+        if selection.get("table"):
+            data = pd.DataFrame(selection["table"])
+            table = pd.read_json(selection["table"])
+            st.write(table)
+        #table = st_ace(value=data, language="json", key="my_table")
+        #if table:
+        #    data = pd.read_json(table)
+        st.write(data)
+        
 
-# Displays the user input features
-st.subheader('Analytical Approach')
+    selection = aggrid_interactive_table(num_rows)
+    #if selection:
+    #    st.write("You selected:")
+    #    st.json(selection["selected_rows"])
+
+
+        # Display the interactive table
+        #grid_response = AgGrid(data, gridOptions=grid_options, key="table")
+
+        # Update the DataFrame with the user-entered values
+
+
+        #st.write(data)
+
+    #user_input_features(num_rows)
 
 if uploaded_file1 is not None:
     st.write(df)
